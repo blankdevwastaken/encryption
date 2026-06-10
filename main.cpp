154272e86b5ddaf0812d62ee7d5e67fbd2aca755
blankdevwastaken <blank@blankdev.me>
2026-06-10 15:14:57 -0400

    Added a decryption system & setup asking if you want decryption or encryption

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 const std::string alphabetupper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const std::string alphabetlower = "abcdefghijklmnopqrstuvwxyz";
-std::string encrypt(std::string plaintext, int key);
+std::string encrypt(std::string plaintext, int key);\
+std::string decrypt(std::string ciphertext, int key);
 bool check(char tochek)
 {
     if (isupper(tochek)) {
@@ -17,20 +18,36 @@ bool check(char tochek)
 }
 
 int main() {
-
 	std::string plaintext;
-    int key;
+	std::string choice;
+	int key;
 
-	std::cout << "Enter the plaintext: ";
-	std::cin >> plaintext;
-	std::cout << "Enter the key: ";
-	std::cin >> key;
 
-    std::string ciphertext = encrypt(plaintext, key);
-	std::cout << "Ciphertext: " << ciphertext << std::endl;
-	std::ofstream KeyFile("key.txt");
-	KeyFile << "Your key is : " << key << std::endl;
-	KeyFile.close();
+	std::cout << "Welcome to the Caesar Cipher program!" << std::endl;
+	std::cout << "Please enter if you want to encrypt/decrypt: ";
+	std::cin >> choice;
+	if (choice == "decrypt") {
+		std::string ciphertext;
+		std::cout << "Enter the ciphertext: ";
+		std::cin >> ciphertext;
+		std::cout << "Enter the key: ";
+		std::cin >> key;
+		std::string plaintext = decrypt(ciphertext, key);
+		std::cout << "Decrypted plaintext: " << plaintext << std::endl;
+	}
+	else {
+		std::cout << "Enter the plaintext: ";
+		std::cin >> plaintext;
+		std::cout << "Enter the key: ";
+
+		std::cin >> key;
+
+		std::string ciphertext = encrypt(plaintext, key);
+		std::cout << "Ciphertext: " << ciphertext << std::endl;
+		std::ofstream KeyFile("key.txt");
+		KeyFile << "Your key is : " << key << std::endl;
+		KeyFile.close();
+	}
 }
 
 std::string encrypt(std::string plaintext, int key) {
@@ -52,4 +69,26 @@ std::string encrypt(std::string plaintext, int key) {
     }
 
     return ciphertext;
+}
+std::string decrypt(std::string ciphertext, int key) {
+	std::string plaintext = "";
+
+	for (int i = 0; i < ciphertext.length(); i++) {
+		char c = ciphertext[i];
+		bool type = check(c);
+		for (int j = 0; j < 26; j++) {
+				if (type && c == alphabetupper[j]) {
+					plaintext += alphabetupper[(j - key + 26) % 26];
+					break;
+				}
+				else if (!type && c == alphabetlower[j]) {
+					plaintext += alphabetlower[(j - key + 26) % 26];
+					break;
+				}
+		}
+	}
+
+	return plaintext;
+
+
 }
